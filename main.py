dcc131e7cee71f1579af28237f16195cee96ca92
Inferno-idk-what-to-do <matthewfiedler19@gmail.com>
2025-03-06 15:11:30 -0500

    make main fn

--- main.py
+++ main.py
@@ -3,35 +3,38 @@ from sshtunnel import SSHTunnelForwarder
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+def main():
+    # load sensitive info
+    load_dotenv()
+    username = os.getenv('USERNAME')
+    password = os.getenv('PASSWORD')
+    dbName = "p32001_34"
 
-username = os.getenv('USERNAME')
-password = os.getenv('PASSWORD')
-dbName = "p32001_34"
+    try:
+        with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                                ssh_username=username,
+                                ssh_password=password,
+                                remote_bind_address=('127.0.0.1', 5432)) as server:
+            server.start()
+            print("SSH tunnel established")
+            params = {
+                'database': dbName,
+                'user': username,
+                'password': password,
+                'host': 'localhost',
+                'port': server.local_bind_port
+            }
 
+            conn = psycopg2.connect(**params)
+            curs = conn.cursor()
+            print("Database connection established")
 
-try:
-    with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
-                            ssh_username=username,
-                            ssh_password=password,
-                            remote_bind_address=('127.0.0.1', 5432)) as server:
-        server.start()
-        print("SSH tunnel established")
-        params = {
-            'database': dbName,
-            'user': username,
-            'password': password,
-            'host': 'localhost',
-            'port': server.local_bind_port
-        }
+            curs.execute('SELECT * FROM movie')
+            print(curs.fetchall())
 
-        conn = psycopg2.connect(**params)
-        curs = conn.cursor()
-        print("Database connection established")
+            conn.close()
+    except:
+        print("Connection failed")
 
-        curs.execute('SELECT * FROM movie')
-        print(curs.fetchall())
-
-        conn.close()
-except:
-    print("Connection failed")
+if __name__ == '__main__':
+    main()
