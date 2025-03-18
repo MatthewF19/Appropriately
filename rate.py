d4c472d277d191f50cae492bf97c8ddda96213fd
HarrisMusungu <hmusungu@ashland.edu>
2025-03-18 15:16:07 -0400

    rate movies implemented

---
+++ rate.py
@@ -0,0 +1,13 @@
+def rate_movie(conn, user_id, movie_id, rating):
+    if not (0 <= rating <= 5):
+        raise ValueError("Rating must be between 0 and 5")
+
+    with conn.cursor() as cursor:
+        query = """
+        INSERT INTO rates (userID, movieID, rating)
+        VALUES (%s, %s, %s)
+        ON CONFLICT (userID, movieID) DO UPDATE SET rating = excluded.rating;"""
+        cursor.execute(query, (user_id, movie_id, rating))
+        conn.commit()
+
+    print(f"User {user_id} rated movie {movie_id} with {rating} stars.")
