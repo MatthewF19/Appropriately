88a839045ac082082ff92664344f4a56ea0bc689
Inferno-idk-what-to-do <matthewfiedler19@gmail.com>
2025-03-20 15:15:50 -0400

    extract prompt into separate fn and bugfixing

--- main.py
+++ main.py
@@ -32,6 +32,48 @@ def commands():
     print("Unfollow user:                   UU")
     print("Exit:                            Q")
 
+def prompt(conn, curs, userid):
+    action = input("What do you want to do?\n-> ").upper()
+    while(action != "Q"):
+        match action:
+            case "CC":
+                create_collection(conn, curs, userid)
+            case "LC":
+                view_collections(conn, curs, userid) 
+            case "PC":
+                print("NOT IMPLEMENTED")
+            case "AC":
+                add_movie(conn, curs, userid)
+            case "RMC":
+                delete_movie(conn, curs, userid)
+            case "RNC":
+                rename_collection(conn, curs, userid)
+            case "DC":
+                delete_collection(conn, curs, userid)
+            case "SM":
+                print("NOT IMPLEMENTED")
+            case "RM":
+                # TODO make fn prompt for movieid/rating
+                rate_movie(conn, userid, movieid, rating)
+            case "PM":
+                print("NOT IMPLEMENTED")
+            case "SU":
+                # TODO make fn prompt for email
+                search_user_by_email(conn, email)
+            case "FU":
+                # TODO make fn prompt for ids
+                follow_user(conn, frid, fdid)
+            case "UU":
+                # TODO make fn prompt for ids
+                unfollow_user(conn, frid, fdid)
+            case "Q":
+                # nop
+                print("", end="") 
+            case _: 
+                commands()
+    
+        action = input("-> ").upper()
+
 def main():
     # load sensitive info
     load_dotenv()
@@ -59,50 +101,8 @@ def main():
             print("Database connection established")
 
             userid = login(conn, curs)
-
-            action = input("What do you want to do?\n-> ").upper()
-            while(action != "Q"):
-                match action:
-                    case "CC":
-                        create_collection(conn, curs, userid)
-                    case "LC":
-                        view_collections(conn, curs, userid) 
-                        print("NOT IMPLEMENTED")
-                    case "PC":
-                        print("NOT IMPLEMENTED")
-                    case "AC":
-                        add_movie(conn, curs, userid)
-                        print("NOT IMPLEMENTED")
-                    case "RMC":
-                        delete_movie(conn, curs, userid)
-                        print("NOT IMPLEMENTED")
-                    case "RNC":
-                        rename_collection(conn, curs, userid)
-                    case "DC":
-                        delete_collection(conn, curs, userid)
-                    case "SM":
-                        print("NOT IMPLEMENTED")
-                    case "RM":
-                        # TODO make fn prompt for movieid/rating
-                        rate_movie(conn, userid, movieid, rating)
-                    case "PM":
-                        print("NOT IMPLEMENTED")
-                    case "SU":
-                        # TODO make fn prompt for email
-                        search_user_by_email(conn, email)
-                    case "FU":
-                        # TODO make fn prompt for ids
-                        follow_user(conn, frid, fdid)
-                    case "UU":
-                        # TODO make fn prompt for ids
-                        unfollow_user(conn, frid, fdid)
-                    case "Q":
-                        # nop
-                        print("", end="") 
-                    case _: 
-                        commands()
-            
-                action = input("-> ")
+            print(userid)
+            prompt(conn, curs, userid)
 
             conn.close()
     except Exception as e:
